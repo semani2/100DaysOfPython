04f4cda001ba85f9eb69c15308214082a2f16018
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2021-12-24 21:46:34 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello World! Welcome to 100 Days of Python")
+print("\nWelcome to the Band Name Generator\n")
+
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print("Your band name could be: " + city_name + " " + pet_name)
+
+
